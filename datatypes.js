46fabff449ff745d1480d07631f4565db85c948a
JahanKumar-b <jahansangeetha916@gmail.com>
2026-01-06 23:12:32 +0530

    day1

---
+++ datatypes.js
@@ -0,0 +1,25 @@
+//primitive datatypes
+//string,number,boolean,null,undefined,bigInt,symbol
+
+let name = 'jahan'
+let score = 69
+let age = true
+let city =null 
+let num = 18383883838n
+console.log(typeof num);
+
+//non-primitive datatypes
+//arrays,object,functions.
+
+//1.functions
+
+const name = 'jahan kumar'
+function logGlobalName(){
+    console.log(name)
+}
+
+console.log(name)
+logGlobalName()
+
+//2.arrays
+
