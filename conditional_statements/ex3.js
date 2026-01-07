15196a37dbb29fc25e14290c026b6d2a29fdd82c
JahanKumar-b <jahansangeetha916@gmail.com>
2026-01-07 19:50:50 +0530

    day2

---
+++ conditional_statements/ex3.js
@@ -0,0 +1,10 @@
+// are there any cookies left ?
+
+var any_cookies_left = true
+if(any_cookies_left)
+{
+    console.log("would you like a cookie?")
+}
+else{
+    console.log("time to bake more cookies")
+}
---
+++ conditional_statements/ex4.js
@@ -0,0 +1,8 @@
+var did_you_brush= false
+if(did_you_brush)
+{
+    console.log("your teeth are clean");
+}
+else{
+    console.log("go brush your teeth now")
+}
