fca04d61536125cc62c089e627653e08725b3b78
JahanKumar-b <jahansangeetha916@gmail.com>
2026-01-07 13:41:32 +0530

    day2

---
+++ basics/ex3.js
@@ -0,0 +1,9 @@
+//functons
+
+var a=10
+var b= 20
+function add()
+{
+    console.log(a+b);
+}
+add()
---
+++ function.js
@@ -0,0 +1,23 @@
+function samsung()
+{ 
+    console.log("this is samsung")
+}
+ 
+function oppo()
+{
+    console.log("this is oppo")
+}
+
+function apple()
+{
+    console.log("this is iphone")
+}
+
+function redmi()
+{
+    console.log("this is redmi")
+}
+samsung()
+redmi()
+apple()
+oppo()
